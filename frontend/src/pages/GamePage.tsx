c310f174f866b9258cf06eaaf448f13b65af2e95
hafemann <hafemann@sipgate.de>
2024-09-29 15:52:59 +0200

    :necktie: implement basic non failsafe game logic

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,13 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import { Dev } from 'components/Dev'
+import { useBlockQuery } from 'queries/BlockQuery'
+import { useEffect, useMemo, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import { Trial } from 'types'
 import { CenteringContainer } from '../components/CenteringContainer'
 import { Matrix } from '../components/Matrix'
+
+type TimeState = 'fixation' | 'queue'
 
 export function GamePage() {
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
   const experimenteeId = searchParams.get('id')
-  const [activeId, setActiveId] = useState<number | undefined>(undefined)
+  const [n, setN] = useState<number>(1)
+
+  const { data, isLoading, status, refetch } = useBlockQuery(n)
+
+  const [currentTrialIndex, setCurrentTrialIndex] = useState<
+    number | undefined
+  >(undefined)
+
+  const [timeState, setTimeState] = useState<TimeState | undefined>(undefined)
+
+  const currentTrial = useMemo<Trial | undefined>(() => {
+    if (currentTrialIndex === undefined || !data) return undefined
+    return data.trials[currentTrialIndex]
+  }, [currentTrialIndex, data])
+
+  console.log(currentTrial, currentTrialIndex)
 
   useEffect(() => {
     if (!experimenteeId) {
@@ -15,9 +35,30 @@ export function GamePage() {
     }
   }, [])
 
-  function bla() {
-    setActiveId(Math.round(Math.random() * 16))
-  }
+  useEffect(() => {
+    if (!currentTrial || currentTrialIndex === undefined || !timeState) return
+
+    if (timeState === 'fixation') {
+      setTimeout(() => {
+        setTimeState('queue')
+      }, currentTrial.ms_vision_time)
+      return
+    }
+
+    if (timeState === 'queue') {
+      setTimeout(() => {
+        setTimeState('fixation')
+        setCurrentTrialIndex(currentTrialIndex + 1)
+      }, currentTrial.ms_fixation_time)
+      return
+    }
+  }, [currentTrialIndex, currentTrial, timeState])
+
+  useEffect(() => {
+    if (status !== 'success') return
+    setCurrentTrialIndex(0)
+    setTimeState('fixation')
+  }, [status])
 
   return (
     <CenteringContainer>
@@ -26,13 +67,26 @@ export function GamePage() {
           'flex h-screen w-screen flex-col items-center justify-center'
         }
       >
-        <Matrix activeId={activeId} />
+        {isLoading && <div>Loading...</div>}
+        <Dev
+          values={{
+            currentTrialIndex: String(currentTrialIndex),
+            timeState,
+            n,
+            blockLength: data?.trials.length,
+            visionBlock: currentTrial?.vision,
+            shouldClickVision: currentTrial?.f_vision_correct ? 'yes' : 'no'
+          }}
+        />
+        <Matrix
+          activeId={timeState === 'fixation' ? currentTrial?.vision : undefined}
+        />
         <div>
           <button
             className={
               'mt-4 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600'
             }
-            onClick={bla}
+            onClick={() => void refetch()}
           >
             bla
           </button>
