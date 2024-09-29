5ae58ee59899862a4581d00e3b0146654507c98f
hafemann <hafemann@sipgate.de>
2024-09-29 15:52:35 +0200

    :construction: add tanstack query client provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { IdInputPage } from './pages/IdInputPage'
 import { PreparationPage } from './pages/PreparationPage'
 import { GamePage } from './pages/GamePage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path={'/'} element={<IdInputPage />} />
-          <Route path={'/prepare'} element={<PreparationPage />} />
-          <Route path={'/game'} element={<GamePage />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<IdInputPage />} />
+            <Route path={'/prepare'} element={<PreparationPage />} />
+            <Route path={'/game'} element={<GamePage />} />
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   )
 }
