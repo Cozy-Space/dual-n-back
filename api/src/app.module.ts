14673b2b801df3e377b49dfcda82bd8c6db0590c
Benjamin Boll | Benji ツ <boll@sipgate.de>
2024-10-26 17:25:50 +0200

    🔥 delete unused files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DualNBackModule } from './dual-n-back/dual-n-back.module';
 import { ConfigModule } from './config/config.module';
 import { UserReactionModule } from './user-reaction/user-reaction.module';
 
 @Module({
   imports: [DualNBackModule, ConfigModule, UserReactionModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
