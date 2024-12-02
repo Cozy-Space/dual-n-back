b0d7cd0841f743bbbb420e5987361f3981e230bd
Benjamin Boll | Benji ツ <boll@sipgate.de>
2024-12-02 14:15:26 +0100

    🐛 fix config type

--- frontend/src/queries/ConfigQuery.tsx
+++ frontend/src/queries/ConfigQuery.tsx
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import { Config } from 'types'
+import { DayConfig } from 'types'
 
 export const useConfigQuery = () => {
   return useQuery({
     queryKey: ['config'],
     queryFn: async () => {
-      return (await fetch('/api/config/get')).json() as unknown as Config
+      return (await fetch('/api/config/get')).json() as unknown as DayConfig
     },
     refetchOnReconnect: false,
     refetchOnWindowFocus: false
